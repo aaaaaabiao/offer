406b194109a2676c107fda78393d047049a1dc82
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2018-12-21 10:54:14 +0800

    链表的第一个公共节点

--- src/offer/FindFirstCommonNode.java
+++ src/offer/FindFirstCommonNode.java
@@ -1,4 +1,60 @@
 package offer;
 
 public class FindFirstCommonNode {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        int len1 = 0;
+        int len2 = 0;
+        ListNode tmp1 = pHead1;
+        ListNode tmp2 = pHead2;
+        while (tmp1 != null){
+            len1++;
+            tmp1 = tmp1.next;
+        }
+        while (tmp2 != null){
+            len2++;
+            tmp2 = tmp2.next;
+        }
+
+
+        if (len1 > len2){
+            int val = len1 - len2;
+            while (val > 0){
+                pHead1 = pHead1.next;
+                val--;
+            }
+
+            while (pHead1 != null){
+                if (pHead1 == pHead2){
+                    return pHead1;
+                }
+                pHead1 = pHead1.next;
+                pHead2 = pHead2.next;
+            }
+        }else{
+            int val = len2 - len1;
+            while (val > 0){
+                pHead2 = pHead2.next;
+                val--;
+            }
+
+            while (pHead2 != null){
+                if (pHead1 == pHead2){
+                    return pHead1;
+                }
+                pHead1 = pHead1.next;
+                pHead2 = pHead2.next;
+            }
+
+        }
+
+        return null;
+    }
 }
