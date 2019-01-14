8c3408764663601bd4ba3a9af3945d8e00c2fcf5
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2019-01-14 14:30:52 +0800

    两个数的平方和

--- src/leetcode/twopoint/JudgeSquareSum.java
+++ src/leetcode/twopoint/JudgeSquareSum.java
@@ -1,4 +1,24 @@
 package leetcode.twopoint;
-
+/**
+ * @version: V1.0
+ * @author: abiao
+ * @className: JudgeSquareSum
+ * @packageName: leetcode.twopoint
+ * @description: 两树的平方和
+ * @data: 2019/1/14
+ **/
 public class JudgeSquareSum {
+    public boolean judgeSquareSum(int c) {
+        int n = (int)Math.sqrt(c);
+        int i = 0;
+        int j = n;
+
+        while (i <= j){
+            int sum = i*i + j*j;
+            if (sum == c) return true;
+            else if (sum < c) i++;
+            else j--;
+        }
+        return false;
+    }
 }
