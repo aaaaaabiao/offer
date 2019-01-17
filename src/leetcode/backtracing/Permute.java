4d73b5705055b50d6eb005b49be056175570373e
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2019-01-17 15:15:37 +0800

    组合

---
+++ src/leetcode/backtracing/Permute.java
@@ -0,0 +1,46 @@
+package leetcode.backtracing;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @version: V1.0
+ * @author: abiao
+ * @className: Permute
+ * @packageName: leetcode.backtracing
+ * @description: 组合
+ * @data: 2019/1/17
+ **/
+public class Permute {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> finalRet = new ArrayList<>();
+        List<Integer> ls = new ArrayList<>();
+        boolean[] visit = new boolean[nums.length];
+        backtracing(ls,finalRet,visit,nums);
+        return finalRet;
+    }
+
+    private static void backtracing(List<Integer> ls, List<List<Integer>> finalRet,
+                             boolean[] visit,int[] nums){
+        if (ls.size() == nums.length){
+            finalRet.add(new ArrayList<>(ls));
+            return;
+        }
+
+        for (int i = 0; i < visit.length; i++){
+            if (visit[i]) continue;
+            visit[i] = true;
+            ls.add(nums[i]);
+            backtracing(ls,finalRet,visit,nums);
+            ls.remove(ls.size()-1);
+            visit[i] = false;
+        }
+    }
+
+    public static void main(String[] args){
+        int[] nums = new int[]{1,2,3};
+        List finalRet = permute(nums);
+
+        int a;
+    }
+
+}
