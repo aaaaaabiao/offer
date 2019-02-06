eb00644899b461bab5eb166c2ac4fa612d48af58
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2019-02-06 17:13:36 +0800

    对折链表 ;

--- src/offer/EntryNodeOfLoop.java
+++ src/offer/EntryNodeOfLoop.java
@@ -34,4 +34,63 @@ public class EntryNodeOfLoop {
         }
         return null;
     }
+
+
+    public static ListNode foldList(ListNode head){
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        //找出中点
+        while (p2 != null && p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        if (p1 == p2){
+            return head;
+        }
+
+        //翻转后半部分链表
+        ListNode p = null;
+        while (p1 != null){
+            ListNode tmp = p1;
+            p1 = p1.next;
+            tmp.next = p;
+            p = tmp;
+        }
+
+        ListNode h = head;
+        ListNode res = new ListNode(-1);
+        while (true){
+            ListNode t1 = h;
+            ListNode t2 = p;
+            h = h.next;
+            p = p.next;
+            t1.next = t2;
+            res.next = t1;
+            res = t2;
+
+            if (p.next == null){
+                res.next = p;
+                break;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+
+        ListNode res = foldList(l1);
+        int a = 0;
+    }
 }
