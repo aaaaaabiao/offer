ef1e58cc79e3183c4d9124922354e1bc2ba0d920
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2019-02-17 10:34:38 +0800

    unique-paths

--- src/leetcode/dp/UniquePaths.java
+++ src/leetcode/dp/UniquePaths.java
@@ -30,4 +30,45 @@ public class UniquePaths {
         }
         return dp[n-1][m-1];
     }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int [][] dp = new int[n][m];
+
+        boolean flag = false;
+        for (int i = 0; i < m; i++){
+            if (obstacleGrid[0][i] == 1 || flag){
+                dp[0][i] = 0;
+                flag = true;
+            }else {
+                dp[0][i] = 1;
+            }
+        }
+
+        flag = false;
+        for (int i = 0; i < n; i++){
+            if (obstacleGrid[i][0] == 1 || flag){
+                dp[i][0] = 0;
+                flag = true;
+            }else {
+                dp[i][0] = 1;
+            }
+
+        }
+
+        if (m > 1 && n > 1){
+            for (int i = 1; i < n; i++){
+                for (int j = 1; j < m; j++){
+                    if (obstacleGrid[i][j] != 1){
+                        dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                    }else {
+                        dp[i][j] = 0;
+                    }
+
+                }
+            }
+        }
+       return dp[n-1][m-1];
+    }
 }
