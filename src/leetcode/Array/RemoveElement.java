c9bd928f63f16accc4effffd728ccf2a601773fb
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2019-02-17 10:37:33 +0800

    删除重复元素

--- src/leetcode/Array/RemoveElement.java
+++ src/leetcode/Array/RemoveElement.java
@@ -1,4 +1,28 @@
 package leetcode.Array;
-
+/**
+ * @version: V1.0
+ * @author: abiao
+ * @className: RemoveElement
+ * @packageName: leetcode.Array
+ * @description: 删除重复元素
+ * @data: 2019/2/17
+ **/
 public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 0;
+        int len = nums.length;
+        while (p < len && q < len){
+            if (nums[q] != val){
+                nums[p++] = nums[q];
+            }
+            q++;
+        }
+        return p;
+    }
+
+    public static void main(String[] args){
+
+    }
 }
