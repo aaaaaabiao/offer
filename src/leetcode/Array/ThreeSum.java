1da9c917f4ce4640f559bd23248d34b185e20214
abiaohubiao_i <abiaohubiao_i@didichuxing.com>
2019-02-19 11:25:34 +0800

    三数之和

---
+++ src/leetcode/Array/ThreeSum.java
@@ -0,0 +1,53 @@
+package leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * @version: V1.0
+ * @author: abiao
+ * @className: ThreeSum
+ * @packageName: leetcode.Array
+ * @description: 三数之和
+ * @data: 2019/2/18
+ **/
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length-2; i++){
+            if (i > 0 && nums[i-1] == nums[i]) continue;
+
+            int p = i+1;
+            int q = nums.length-1;
+            int target = 0 - nums[i];
+            while (p < q){
+                if (p > i+1 && nums[p] == nums[p-1]){
+                    p++;
+                    continue;
+                }
+                if (q < nums.length-1 && nums[q] == nums[q+1]){
+                    q--;
+                    continue;
+                }
+                if (nums[p] + nums[q] == target){
+                    List ls = Arrays.asList(nums[i],nums[p],nums[q]);
+                    res.add(ls);
+                    p++;
+                    q--;
+                }else if (nums[p] + nums[q] > target){
+                    q--;
+                }else {
+                    p++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[] nums = new int[]{1,-1,-1,0};
+        List res = threeSum(nums);
+        int a = 0;
+    }
+}
