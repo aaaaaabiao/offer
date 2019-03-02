7f785d01d069a833fc621e52d0598c7864c248bb
hubiao <736813089@qq.com>
2019-03-02 10:47:00 +0800

    数组中超过一半的数字修改

--- src/offer/MoreThanHalfNum.java
+++ src/offer/MoreThanHalfNum.java
@@ -31,6 +31,29 @@ public class MoreThanHalfNum {
         return 0;
     }
 
+    //时间复杂度O(n)
+    public int MoreThanHalfNum_Solution(int [] array) {
+        int majority = array[0];
+        int count = 1;
+        for(int i = 1; i < array.length; i++){
+            count = majority == array[i] ? count+1 : count-1;
+            if(count == 0){
+                majority = array[i];
+                count = 1;
+            }
+        }
+
+        int len = array.length / 2;
+        count = 0;
+        for(int val : array){
+            if(majority == val){
+                count++;
+            }
+        }
+
+        return count > len ? majority : 0;
+    }
+
     public static void main(String[] args){
         int[] array = new int[]{1,2,3,2,2,2,5,4,2};
         moreThanHalfNum(array);
