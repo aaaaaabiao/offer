16f52e980bfb0201f2981f50f08a023aa7fe6ca5
hubiao <736813089@qq.com>
2019-04-01 20:21:33 +0800

    动态规划实现正则表达式的匹配

--- src/offer/Match.java
+++ src/offer/Match.java
@@ -9,7 +9,7 @@ package offer;
  ````````````````````````````````````链接`````````````````````````````````````
  https://www.nowcoder.com/practice/45327ae22b7b413ea21df13ee7d6429c?tpId=13&tqId=11205&tPage=3&rp=3&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
  ``````````````````````````````````解题思路```````````````````````````````````
- 方法：
+ 方法：递归
  描述：用si和pi分别代表字符串str和模式pattern当前的位置
  1.如果当pattern[pi+1] = '*'
     1.str[si] == pattern[pi] || pattern[pi] == '.'。说明第一个字符匹配，这时候有三种情况
@@ -21,6 +21,21 @@ package offer;
         模式后移两位，pi = pi+2。也就是视x*匹配0个字符。
  2.如果当str[si] == pattern[pi] || pattern[pi] == '.',这个代表字符串这个字符可以被模式匹配('.'可以代表任意字符)。s1++,pi++,接着往下匹配。
  3.否则返回false
+
+
+
+ 方法:动态规划
+ 描述：dp[i][j]:代表字符串前i个字符与模式前j个字符是否匹配。
+ 转移方程为:
+ 1.s[i] == p[j] || p[j] == '.' ==> dp[i][j] = dp[i-1][j-1]
+ 2.p[j] == '*'
+    如果s[i] == p[j-1] || p[j] == '.'
+        dp[i][j] = dp[i-1][j] 代表*匹配一个或多个字符。
+                    || dp[i][j-1]代表*匹配一字符
+                    || dp[i][j-2]代表*匹配空字符
+    否则
+        dp[i][j] = dp[i][j-2]
+
  ````````````````````````````````````思考`````````````````````````````````````
     1.必须要先检查后一个字符是不是*。
     2.做边界判断。
@@ -35,7 +50,7 @@ public class Match {
             return false;
         int strIndex = 0;
         int patternIndex = 0;
-        return matchCore(str, strIndex, pattern, patternIndex);
+        return matchCoredp(str, strIndex, pattern, patternIndex);
 
     }
     private static boolean matchCore(char[] str,int si, char[] pattern,int pi) {
@@ -48,7 +63,7 @@ public class Match {
                 return matchCore(str,si,pattern,pi+2) || matchCore(str,si+1,pattern,pi+2)
                         || matchCore(str,si+1,pattern,pi);
             }else{
-                matchCore(str,si,pattern,pi+2);
+                return matchCore(str,si,pattern,pi+2);
             }
         }
 
@@ -59,7 +74,41 @@ public class Match {
         }
 
     }
+
+
+
+    private static boolean matchCoredp(char[] str,int si, char[] pattern,int pi) {
+        int slen = str.length;
+        int plen = pattern.length;
+        boolean [][] dp = new boolean[slen+1][plen+1];
+
+        dp[0][0] = true;
+        for (int i = 0; i < pattern.length; i++) {
+            if (pattern[i] == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+
+
+        for (int i = 0; i < slen; i++){
+            for (int j = 0; j < plen; j++){
+                if(j > 0 && pattern[j] == '*'){
+                    if(str[i] == pattern[j-1] || pattern[j-1] == '.'){
+                        dp[i+1][j+1] = dp[i][j+1] || dp[i+1][j] || dp[i+1][j-1];
+                    }else{
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    }
+                }
+
+                if(str[i] == pattern[j] || pattern[j] == '.'){
+                    dp[i+1][j+1] = dp[i][j];
+                }
+            }
+        }
+        return dp[slen][plen];
+    }
     public static void main(String[] args){
-        System.out.println(match(new char[]{},new char[]{'.','*'}));
+//        System.out.println(match("aab".toCharArray(),"c*a*b".toCharArray()));
+        System.out.println(match("aab".toCharArray(),"c*a*b".toCharArray()));
     }
 }
