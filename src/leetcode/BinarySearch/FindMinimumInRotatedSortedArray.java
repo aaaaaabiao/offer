b0f41ad55a68f193135673cddca1f6355cccbac2
hubiao <736813089@qq.com>
2019-08-22 21:36:23 +0800

    排序数组中目标值

--- src/leetcode/BinarySearch/FindMinimumInRotatedSortedArray.java
+++ src/leetcode/BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -53,13 +53,59 @@ public class FindMinimumInRotatedSortedArray {
         }
         return nums[l];
     }
+    /**
+     * 旋转数组找到指定的值3
+     *
+     * @param
+     * @param
+     * @param
+     * @return
+     */
+    public static int searchTarget(int[] arr, int t) {
+        if (arr == null || arr.length == 0){
+            return -1;
+        }
+
+        int l = 0;
+        int h = arr.length - 1;
+        while (l <= h){
+            int mid = l + (h - l)/2;
+            if (arr[mid] == t){
+                return mid;
+            }else if (arr[l] < arr[mid]){
+                if (arr[l] <= t && t < arr[mid]){
+                    h = mid - 1;
+                }else {
+                    l = mid + 1;
+                }
+            }else if (arr[l] > arr[mid]){
+                if (arr[mid] < t && t <= arr[h]){
+                    l = mid + 1;
+                }else {
+                    h = mid - 1;
+                }
+            }else {
+                if (arr[mid] == arr[h]){
+                    l++;
+                    h--;
+                }else {
+                    l = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
 
     public static void main(String[] args){
-//        int[] nums = new int[]{3,4,5,1,2};
-        int[] nums1 = new int[]{3,1,2};
-        System.out.println(findMin(nums1));
+        int[] nums = new int[]{3,4,5,1,2,2,2,2,2,2};
 
-        int[] nums2 = new int[]{3,3,1,3};
-        System.out.println(findMin2(nums2));
+        System.out.println(searchTarget(nums, 5));
+
+//        int[] nums1 = new int[]{3,1,2};
+//        System.out.println(findMin(nums1));
+//
+//        int[] nums2 = new int[]{3,3,1,3};
+//        System.out.println(findMin2(nums2));
     }
 }
